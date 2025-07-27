f05d01640ec7aec5a4aa232f9050f1b1460a4c35
alepuscas <alepuscas04@gmail.com>
2025-07-27 12:23:46 +0300

    Added data

---
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -0,0 +1,34 @@
+package guru.springframework.controller;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+public class IndexController {
+
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"","/","/index"})
+    public String index(Model model) {
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("Category: " + category.get().getId());
+        System.out.println("UnitOfMeasure: " + unitOfMeasure.get().getId());
+
+        return "index";
+
+    }
+}
