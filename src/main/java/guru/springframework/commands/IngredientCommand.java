fe6907c94bfa12dd7b6f92d040b51db1ca8db85f
alepuscas <alepuscas04@gmail.com>
2025-07-30 13:51:03 +0300

    Update recipe functionality

--- src/main/java/guru/springframework/commands/IngredientCommand.java
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -6,6 +6,9 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
+/**
+ * Created by jt on 6/21/17.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,6 +16,6 @@ public class IngredientCommand {
     private Long id;
     private String description;
     private BigDecimal amount;
-    private UnitOfMeasureCommand unitOfMeasure;
+    private UnitOfMeasureCommand uom;
 
 }
